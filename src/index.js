afd6db269d496901bdc3cb599b328618726b18ec
Timothy Parker <tlparker71@gmail.com>
2022-11-10 17:20:02 +0000

    uri change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
 <Auth0Provider
     domain={process.env.REACT_APP_DOMAIN}
     clientId={process.env.REACT_APP_CLIENTID}
-    redirectUri='https://reliable-cucurucho-3ced6f.netlify.app/login/callback'
+    redirectUri='https://dev-zlkci082.us.auth0.com/login/callback'
   >
     <App />
   </Auth0Provider>
